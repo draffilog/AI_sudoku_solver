0021b4183414c76d4c2f5c966c76607199e4eb5f
Draffilog <116096032+draffilog@users.noreply.github.com>
2023-07-09 18:17:34 +0100

    commit message

---
+++ AI_sudoku_solver.py
@@ -0,0 +1,10 @@
+import numpy as np
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.optimizers import Adam
+class SudokuSolver:
+import numpy as np
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.optimizers import Adam
+class SudokuSolver:
